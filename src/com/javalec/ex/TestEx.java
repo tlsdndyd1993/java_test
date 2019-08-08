94fac566c98de6ccc96bf1ab839fb8684d7a27d3
tlsdndyd1993 <tlsdndyd1993@naver.com>
2019-08-09 00:21:57 +0900

    Add Test File [TestEx.java]

---
+++ src/com/javalec/ex/TestEx.java
@@ -0,0 +1,21 @@
+package com.javalec.ex;
+
+public class TestEx {
+	public static void main(String[] args) {
+		int i = 29;
+		int j = 30;
+		int h = 30;
+		
+		char c = 'A';
+		char d = 'a';
+		char e = 'B';
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(h);
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
